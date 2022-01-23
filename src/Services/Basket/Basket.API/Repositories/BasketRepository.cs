76d5fa94be34ee0ed2fdb7e5e24a5739182224ad
sudeeshmohan <47549069+sudeeshmohan@users.noreply.github.com>
2022-01-23 18:43:30 +0530

    basket microservices

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCasche;
+
+        public BasketRepository(IDistributedCache redisCasche)
+        {
+            _redisCasche = redisCasche;
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCasche.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCasche.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCasche.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
